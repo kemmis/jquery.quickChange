c2bb776a09d0416b3dec423786125793e17247f2
Rafe Kemmis <rafe@kemmis.info>
2014-11-03 09:07:38 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 jquery.quickChange
 ==================
+Adapted from this stackoverflow answer: http://stackoverflow.com/a/7284309/27497
---
+++ jquery.quickchange.js
@@ -0,0 +1,17 @@
+﻿$.fn.quickChange = function (handler) {
+    return this.each(function () {
+        var self = this;
+        self.qcindex = self.selectedIndex;
+        var interval;
+        function handleChange() {
+            if (self.selectedIndex != self.qcindex) {
+                self.qcindex = self.selectedIndex;
+                handler.apply(self);
+            }
+        }
+        $(self).focus(function () {
+            interval = setInterval(handleChange, 100);
+        }).blur(function () { window.clearInterval(interval); })
+        .change(handleChange); //also wire the change event in case the interval technique isn't supported (chrome on android)
+    });
+};
